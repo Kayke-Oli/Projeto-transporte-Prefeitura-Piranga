f4f6717d6f2f18b0832340d6ed27454e1f5786f2
Kayke <kaykerodr5@gmail.com>
2026-07-24 17:48:02 -0300

    Adição da classe Motorista, a qual tem a função de cuidar dos cadastros dos motoristas.

---
+++ src/Motoristarepository.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Entidades.h"
+#include "Database.h"
+#include <pqxx/pqxx>
+#include <optional>
+#include <iostream>
+
+class MotoristaRepository
+{
+private:
+    Database &db;
+
+public:
+    MotoristaRepository(Database &database) : db(database) {}
+
+    // Retorna o ID gerado (>0) em caso de sucesso, ou std::nullopt em caso de erro.
+    std::optional<int> cadastrar(const Motorista &motorista)
+    {
+        try
+        {
+            pqxx::work transacao(*db.getConexao());
+            pqxx::result res = transacao.exec_params(
+                "INSERT INTO Motoristas (nome, cpf) VALUES ($1, $2) RETURNING id_motorista",
+                motorista.nome, motorista.cpf);
+            transacao.commit();
+            int id = res[0][0].as<int>();
+            std::cout << "Motorista cadastrado com sucesso! ID: " << id << std::endl;
+            return id;
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << "Erro ao cadastrar motorista: " << e.what() << std::endl;
+            return std::nullopt;
+        }
+    }
+};
